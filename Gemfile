33bda769996e67ffa02a9bcde953deaf05392684
Gavin Stark <gavin@gstark.com>
2017-04-13 12:00:32 -0400

    Omniauth

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'jquery-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Authentication with oauth
+gem 'omniauth'
+gem 'omniauth-github'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root 'items#index'
+
+  get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
+  get    '/auth/:provider/callback' => 'session#create'
+  get    '/auth/failure'            => 'session#failure'
+
+  get  '/login'  => 'session#new'
+  post '/login'  => 'session#create'
+  get  '/logout' => 'session#destroy'
+
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
